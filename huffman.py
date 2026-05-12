d122a4b08d90cd2a1a5fca8d9545d7afbf8b7d83
abrham17 <abrhamhabtom17@gmail.com>
2026-05-12 10:57:32 +0300

    feat:a basic huffman structure

--- huffman.py
+++ huffman.py
@@ -1,10 +1,26 @@
+from collections import Counter
+from heapq import heappop, heappush
 class HuffmanNode:
     def __init__(self, char=None, freq=0, left=None, right=None):
+        """
+        Initialize a Huffman node
+        
+        Args:
+            char: The character this node represents (None for internal nodes)
+            freq: Frequency of the character
+            left: Left child node
+            right: Right child node
+        """
+
         self.char = char
         self.freq = freq
         self.left = left
         self.right = right
+
     def __lt__(self, other):
+        """
+        Compare two Huffman nodes by frequency
+        """
         return self.freq < other.freq
 
 class HuffmanCoding:
@@ -14,13 +30,18 @@ class HuffmanCoding:
         self.reverse_mapping = {}
     
     def frequency_analysis(self, text):
-        pass
+        return dict(Counter(text))
     
     def build_heap(self, frequency):
-        pass
+        for char, freq in frequency.items():
+            heappush(self.heap, HuffmanNode(char, freq))
     
     def merge_nodes(self):
-        pass
+        while len(self.heap) > 1:
+            node1 = heappop(self.heap)
+            node2 = heappop(self.heap)
+            merged = HuffmanNode(left=node1, right=node2, freq=node1.freq + node2.freq)
+            heappush(self.heap, merged)
     
     def generate_codes(self):
         pass
